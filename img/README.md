c5ae3a6d002939a630311f425bf1baee84f0a62d
Bugrahan Cigdemoglu <bcigdemoglu-i@dnanexus.com>
2016-10-17 18:41:59 -0400

    Update UML and add new API calls

--- img/README.md
+++ img/README.md
@@ -5,4 +5,4 @@ https://drive.google.com/file/d/0B6BJ6FVDB9EmcmoybE1MNXlGekk/view?usp=sharing
 https://drive.google.com/file/d/0B6BJ6FVDB9EmYXk1aTBTcjhnY1E/view?usp=sharing
 
 ###Architecture Link:
-https://www.gliffy.com/go/html5/launch?app=1b5094b0-6042-11e2-bcfd-0800200c9a66
+https://www.gliffy.com/go/html5/11308599
--- web/flask_rest_service/resources.py
+++ web/flask_rest_service/resources.py
@@ -1,59 +1,138 @@
-from flask import request, abort, json
+"""Summary
+"""
+from flask import request, abort, json, session
 import flask_restful as restful
 from flask_restful import reqparse
 from flask_rest_service import app, api, mongo
 from bson.objectid import ObjectId
+from md5 import md5
 
-class Inserter(restful.Resource):
-    def get(self):
-        testing_id = mongo.db.testing.insert({"trial_key" : "trial_val"})
-        return mongo.db.testing.find_one({"_id": testing_id})
+# User: Username <Email>, Password, Friends <List of Usernames>
+#
 
-@app.route('/rem', methods=['GET'])
-def remove_from_database():
-    testing_id = mongo.db.testing.remove({"trial_key": "trial_val"})
-    return testing_id
+class Login(restful.Resource):
+    def post(self):
+        username_form  = request.form['username']
+        user = mongo.db.users.find_one({"username": username_form})
 
-class Finder(restful.Resource):
-    def get(self):
-        return [x for x in mongo.db.testing.find({"trial_key": "trial_val"})]
+        if not user:
+            raise ServerError('Invalid username')
+
+        password_form  = request.form['password']
+        encrypted_pass = md5(password_form).hexdigest()
+
+        if user.password == encrypted_pass:
+            session[username_form] = username_form
+            return {"statusCode": 200, user}
+
+        raise ServerError('Invalid password')
+
+class Register(restful.Resource):
+    def post(self):
+        user = {"username": request.form['username'],
+                "password": md5(request.form['password']).hexdigest(),
+                "name": request.form['username']}
+        mongo.db.users.insert(user)
+        return user
 
 class Root(restful.Resource):
     def get(self):
         return {
             'status': 'OK',
             'mongo': str(mongo.db),
+            mongo.db.users.find()
         }
 
-class ReadingList(restful.Resource):
-    def __init__(self, *args, **kwargs):
-        self.parser = reqparse.RequestParser()
-        self.parser.add_argument('reading', type=str)
-        super(ReadingList, self).__init__()
+'''
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    """Summary
 
-    def get(self):
-        return [x for x in mongo.db.readings.find()]
+    Returns:
+        TYPE: Description
 
-    def post(self):
-        args = self.parser.parse_args()
-        if not args['reading']:
-            abort(400)
-        with open('pylog.txt', 'w') as file_:
-            file_.write(args['reading'])
-        jo = json.loads(args['reading'])
-        reading_id =  mongo.db.readings.insert(jo)
-        return mongo.db.readings.find_one({"_id": reading_id})
+    Raises:
+        ServerError: Description
+    """
+    if 'username' in session:
+        return redirect(url_for('index'))
+
+    error = None
+    try:
+        if request.method == 'POST':
+            username_form  = request.form['username']
+            username = mongo.db.users.find_one({"userId": testing_id})
+
+            if not username:
+                raise ServerError('Invalid username')
+
+            password_form  = request.form['password']
+            cur.execute("SELECT pass FROM users WHERE name = {};"
+                        .format(username_form))
+
+            for row in cur.fetchall():
+                if md5(password_form).hexdigest() == row[0]:
+                    session['username'] = request.form['username']
+                    return redirect(url_for('index'))
+
+            raise ServerError('Invalid password')
+    except ServerError as e:
+        error = str(e)
+
+    return render_template('login.html', error=error)
 
 
-class Reading(restful.Resource):
-    def get(self, reading_id):
-        return mongo.db.readings.find_one_or_404({"_id": reading_id})
+@app.route('/logout')
+def logout(asdasd,das das d):
+    session.pop('username', None)
+    return redirect(url_for('index'))
+'''
 
-    def delete(self, reading_id):
-        mongo.db.readings.find_one_or_404({"_id": reading_id})
-        mongo.db.readings.remove({"_id": reading_id})
-        return '', 204
+# class Inserter(restful.Resource):
+#     def get(self):
+#         testing_id = mongo.db.testing.insert({"trial_key" : "trial_val"})
+#         return mongo.db.testing.find_one({"_id": testing_id})
 
+# @app.route('/rem', methods=['GET'])
+# def remove_from_database():
+#     testing_id = mongo.db.testing.remove({"trial_key": "trial_val"})
+#     return testing_id
+
+# class Finder(restful.Resource):
+#     def get(self):
+#         return [x for x in mongo.db.testing.find({"trial_key": "trial_val"})]
+
+# class ReadingList(restful.Resource):
+#     def __init__(self, *args, **kwargs):
+#         self.parser = reqparse.RequestParser()
+#         self.parser.add_argument('reading', type=str)
+#         super(ReadingList, self).__init__()
+
+#     def get(self):
+#         return [x for x in mongo.db.readings.find()]
+
+#     def post(self):
+#         args = self.parser.parse_args()
+#         if not args['reading']:
+#             abort(400)
+#         with open('pylog.txt', 'w') as file_:
+#             file_.write(args['reading'])
+#         jo = json.loads(args['reading'])
+#         reading_id =  mongo.db.readings.insert(jo)
+#         return mongo.db.readings.find_one({"_id": reading_id})
+
+
+# class Reading(restful.Resource):
+#     def get(self, reading_id):
+#         return mongo.db.readings.find_one_or_404({"_id": reading_id})
+
+#     def delete(self, reading_id):
+#         mongo.db.readings.find_one_or_404({"_id": reading_id})
+#         mongo.db.readings.remove({"_id": reading_id})
+#         return '', 204
+
+api.add_resource(Login, '/login')
+api.add_resource(Register, '/register')
 api.add_resource(Root, '/')
 api.add_resource(ReadingList, '/readings/')
 api.add_resource(Reading, '/readings/<ObjectId:reading_id>')
