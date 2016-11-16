d01f8813db91dbd90c68c395e43f6d5ad613c1cf
Alex <alex.owen@thetileapp.com>
2016-11-16 13:35:58 -0500

    Added an Itinerary Manager class

---
+++ PlanIt/PlanIt/ItineraryManager.swift
@@ -0,0 +1,11 @@
+//
+//  ItineraryManager.swift
+//  PlanIt
+//
+//  Created by Alex Owen on 11/13/16.
+//  Copyright © 2016 oosegroup13. All rights reserved.
+//
+
+import Foundation
+
+var displayName : [ItineraryShell] = []
