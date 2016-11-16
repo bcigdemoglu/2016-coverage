8c3b879b82266fdbaca658ee47bc3e91638a964f
Alex <alex.owen@thetileapp.com>
2016-11-16 13:49:58 -0500

    Added the actual Connection Handler stuff that we need

--- PlanIt/PlanIt/ConnectionHandler.swift
+++ PlanIt/PlanIt/ConnectionHandler.swift
@@ -35,36 +35,28 @@ let defaultHeaders : HTTPHeaders = [
     return statusCode
 }*/
 
-func sendLoginRequest(username: String, password: String, loginController : LoginPageViewController) -> Int{
+func sendLoginRequest(username: String, password: String, completionHandler : @escaping (Int) -> () ) {
     let params: Parameters = [
         "username": username,
         "password": password
     ]
     
-    var statusCode = 0
-    Alamofire.request(baseURL + "login", method: .post, parameters: params, encoding: JSONEncoding.default).validate().responseJSON { response in
-        switch response.result {
-        case .success(let value):
-            let json = JSON(value)
-            print("JSON: \(json)")
-            loginController.performSegue(withIdentifier: "ListViewSegue", sender: nil)
-        case .failure(let error):
-            print(error)
-            statusCode = 1;
-        }
+    Alamofire.request(baseURL + "login", method: .post, parameters: params, encoding: JSONEncoding.default).response { response in
+        print("\(response.data)")
+        let code = (response.response?.statusCode)!
+        completionHandler(code)
     }
-    return statusCode;
-
 }
 
-func sendRegisterRequest(username: String, password: String, name : String) {
+
+/*func sendRegisterRequest(username: String, password: String, name : String) {
     let parameters: Parameters = [
         "username": username,
         "password": password,
         "name" : name
     ]
 
-    //var statusCode = 0
+
     Alamofire.request(baseURL + "login", method: .post, parameters: parameters, encoding: JSONEncoding.default).responseJSON { response in
         print(response.request?.httpBody)  // original URL request
         print(response.response) // HTTP URL response
@@ -75,17 +67,30 @@ func sendRegisterRequest(username: String, password: String, name : String) {
             print("JSON: \(JSON)")
         }
     }
+}*/
+
+func getItineraryListShells(userID : String, completionHandler: @escaping ([Itinerary]?, String) -> ()) {
+    Alamofire.request(baseURL + "itinerarylistshells/" + userID, method: .get, encoding: JSONEncoding.default).responseJSON { response in
+        var array : [Itinerary]? = []
+        switch response.result {
+        case .success(let value):
+            let json = JSON(value)
+            print("JSON: \(json)")
+            
+            for (index,subJson):(String, JSON) in json {
+                let name = subJson[0]["name"].string
+                let uid = subJson[0]["uid"].string
+                let shell = Itinerary(name: name!, uid: uid!)
+                array!.append(shell!)
+            }
+            completionHandler(array, "")
+        case .failure(let error):
+            print(error)
+            completionHandler(array, error.localizedDescription)
+        }
+    }
 }
 
-/*func getItineraryList(username : String, timestamp : u_long) -> [Itinerary]{
-    var array = [Itinerary]()
-    let parameters: Parameters = [
-        "username" : username,
-        "last_sync_timestamp" : timestamp
-    ]
-    Alamofire.request(baseURL + "getItineraryLists", method: .get, parameters : parameters, encoding: JSONEncoding.default).responseJSON { response in
-       
-}*/
 
 
 
