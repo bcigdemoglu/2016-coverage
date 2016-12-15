732e40d8ad578367e5b23bcc43a5e96bc773db1b
ahe3 <ahe3@jhu.edu>
2016-12-15 14:32:47 -0500

    Add navigation items and make drawer visible on each page

---
+++ PlanIt/PlanIt/RatingsViewController.swift
@@ -0,0 +1,39 @@
+//
+//  RatingsViewController.swift
+//  PlanIt
+//
+//  Created by CS Student on 12/15/16.
+//  Copyright © 2016 oosegroup13. All rights reserved.
+//
+
+import UIKit
+
+class RatingsViewController: UIViewController {
+
+    @IBOutlet var menuButton: UIBarButtonItem!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.menuButton.target = self.revealViewController()
+        self.menuButton.action = #selector(SWRevealViewController.revealToggle(_:))
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
