2cdf5933269568120c7021bfbee1ca1e8129a461
ahe3 <ahe3@jhu.edu>
2016-12-19 14:36:43 -0500

    Rate

--- PlanIt/PlanIt/RatingsViewController.swift
+++ PlanIt/PlanIt/RatingsViewController.swift
@@ -8,13 +8,20 @@
 
 import UIKit
 
-class RatingsViewController: UIViewController {
+class RatingsViewController: UIViewController, UIPickerViewDelegate{//, UIPickerViewDataSource {
 
     @IBOutlet var locationName: UILabel!
+    
+    @IBOutlet var ratingPicker: UIPickerView!
+    
+    var pickerData: [String] = [String]()
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        
+        // Input data into Array:
+        pickerData = ["1","2","3","4","5"]
     }
     
     var location = String()
@@ -27,15 +34,31 @@ class RatingsViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    // The number of columns of data
+    func numberOfComponentsInPickerView(pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    // The number of rows of data
+    func pickerView(_pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return pickerData.count
+    }
+    
+    // The data to return for the row and component (column) that's being passed in
+    func pickerView(_pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return pickerData[row]
+    }
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
+        let strDate = ratingPicker.selectedRow(inComponent: <#T##Int#>)
+        
     }
-    */
+    
 
 }
